45b1bc55c8ef9d92e3021afd4ac192236902a201
Jonathan Zhang <Jonathan.Zhang111@gmail.com>
2020-11-13 02:08:10 -0500

    Added input and dictionary reading

--- src/Cryptograms.java
+++ src/Cryptograms.java
@@ -1,24 +1,62 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 /**
  * Cryptograms class for decoding inputted cryptograms using a dictionary
  */
 public class Cryptograms {
-    public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        ArrayList<String> input = new ArrayList<>();
-        while (scan.hasNext()) {
-            input.add(scan.next());
-        }
-        scan.close();
-        File dictionary = new File("dictionary.txt");
-        //BufferedReader dictionary = new BufferedReader(new FileReader("dictionary.txt"));
 
-        int numDecodings;
+    static String input = "";
+    static ArrayList<String> dictionary = new ArrayList<>();
+    static String[] words;
+
+    public static void main(String[] args) {
+
+        char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
         
+        try {
+            Scanner scan = new Scanner(System.in);
+            input = scan.nextLine();
+            scan.close();
+
+            File file = new File("dictionary.txt");
+            Scanner scanFile = new Scanner(file);
+            while (scanFile.hasNextLine()) {
+                dictionary.add(scanFile.nextLine());
+            }
+            scanFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("dictionary.txt not found");
+            System.exit(1);
+        } catch (Exception e) {
+            System.out.println("Input error");
+            System.exit(1);
+        }
+
+        words = input.split(" ");
+        decode();
+        //int numDecodings = 0;
+        words = input.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            
+        }
+    }
+
+    public static String decode() {
+        String[] sortedStrings = sortByLength(words);
+        
+        /*for (int i = 0; i < words.length; i++) {
+            
+        }*/
+    }
+
+    public static String[] getWordsOfLength(int length) {
+        ArrayList<String> validWords = new ArrayList<>();
+        for (String word : dictionary) {
+            validWords.add(word);
+        }
+        return validWords.toArray(new String[validWords.size()]);
     }
 }
